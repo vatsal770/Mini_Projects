cdefa2a34e9f84eac407d10fe45c66898bde411a
vatsal770 <117573668+vatsal770@users.noreply.github.com>
2025-06-02 00:12:57 +0530

    Update app.py
    Changed the dataset path

--- House Explorer and Price Predictor/app.py
+++ House Explorer and Price Predictor/app.py
@@ -8,7 +8,7 @@ from sklearn.linear_model import LinearRegression
 # Loading and preprocessing the dataset
 @st.cache_data
 def load_data():
-    df = pd.read_csv("C:/Users/vatsal trivedi/Documents/VS Code/House_prediction/dataset.csv")
+    df = pd.read_csv("House Explorer and Price Predictor/house_dataset.csv")
 
     # Drop particular columns as it contains duplicate data
     df.drop(columns=['POSTED_BY','UNDER_CONSTRUCTION','RERA'], inplace=True)
